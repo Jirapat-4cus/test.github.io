4201102de584e60c79f16e841efb78665ec4b1e1
pppeeern <kaoklongpeerawas@gmail.com>
2025-03-14 04:07:46 +0700

    index edited + js for category

---
+++ cat.js
@@ -0,0 +1,16 @@
+let favSec = document.querySelector("#favsec");
+let favCon = favSec.querySelector(".section-content");
+
+document.querySelectorAll(".category[fav='true']").forEach(favcat => {
+    favCon.appendChild(favcat.cloneNode(true));
+});
+
+if (favCon.children.length > 0) {
+    favSec.style.display = "block";
+}
+else favSec.style.display = "none";
+
+document.querySelectorAll(".category").forEach(cat => {
+    let tt = cat.querySelector(".category-title").innerHTML;
+    cat.setAttribute("title", tt);
+})
