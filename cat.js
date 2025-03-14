46466da709faac60db95428ef84b811a634a0674
pppeeern <kaoklongpeerawas@gmail.com>
2025-03-14 08:33:23 +0700

    link every page *except login

--- cat.js
+++ cat.js
@@ -1,14 +1,18 @@
-let favSec = document.querySelector("#favsec");
-let favCon = favSec.querySelector(".section-content");
+function updateFav(){
+    let favSec = document.querySelector("#favsec");
+    let favCon = favSec.querySelector(".section-content");
 
-document.querySelectorAll(".category[fav='true']").forEach(favcat => {
-    favCon.appendChild(favcat.cloneNode(true));
-});
+    document.querySelectorAll(".category[fav='true']").forEach(favcat => {
+        favCon.appendChild(favcat.closest("a").cloneNode(true));
+    });
 
-if (favCon.children.length > 0) {
-    favSec.style.display = "block";
+    if (favCon.children.length > 0) {
+        favSec.style.display = "block";
+    }
+    else favSec.style.display = "none";
 }
-else favSec.style.display = "none";
+
+window.onload = updateFav();
 
 document.querySelectorAll(".category").forEach(cat => {
     let tt = cat.querySelector(".category-title").innerHTML;
